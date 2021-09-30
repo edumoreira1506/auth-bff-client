79e5742bd2799eca0f6648348bd47e31c8922899
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-30 00:08:10 -0300

    Bump to 0.1.3

--- src/AuthBffClient.ts
+++ src/AuthBffClient.ts
@@ -57,7 +57,7 @@ export default class AuthBffClient {
 
   async refreshToken(token: string) {
     try {
-      const { data } = await this._axiosAuthBffInstance.post<RequestSuccess>('/refresh-token', {}, {
+      const { data } = await this._axiosAuthBffInstance.post<RequestSuccess>('/v1/refresh-token', {}, {
         headers: {
           'X-Cig-Token': token
         }
